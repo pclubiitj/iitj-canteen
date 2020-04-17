8a1fe4193172b918a7da4a4ac995e3ce309e2a3c
kunaltawatia <tawatia.1@iitj.ac.in>
2020-04-17 21:59:48 +0530

    Lint Fixes

--- server/server/auth/auth.route.js
+++ server/server/auth/auth.route.js
@@ -8,12 +8,12 @@ const config = require('../../config/config');
 const router = express.Router(); // eslint-disable-line new-cap
 
 /** POST /api/auth/login - Returns token if correct username and password is provided */
-router.route('/login')
-  .post(validate(paramValidation.login, {}, {}), authCtrl.login);
+router.route('/login').post(validate(paramValidation.login, {}, {}), authCtrl.login);
 
 /** GET /api/auth/random-number - Protected route,
  * needs token returned by the above as header. Authorization: Bearer {token} */
-router.route('/random-number')
-  .get(expressJwt({ secret: config.jwtSecret }), authCtrl.getRandomNumber);
+router
+	.route('/random-number')
+	.get(expressJwt({ secret: config.jwtSecret }), authCtrl.getRandomNumber);
 
 module.exports = router;
--- server/server/user/user.controller.js
+++ server/server/user/user.controller.js
@@ -4,12 +4,12 @@ const User = require('./user.model');
  * Load user and append to req.
  */
 function load(req, res, next, id) {
-  User.get(id)
-    .then((user) => {
-      req.user = user; // eslint-disable-line no-param-reassign
-      return next();
-    })
-    .catch(e => next(e));
+	User.get(id)
+		.then((user) => {
+			req.user = user; // eslint-disable-line no-param-reassign
+			return next();
+		})
+		.catch((e) => next(e));
 }
 
 /**
@@ -17,7 +17,7 @@ function load(req, res, next, id) {
  * @returns {User}
  */
 function get(req, res) {
-  return res.json(req.user);
+	return res.json(req.user);
 }
 
 /**
@@ -27,14 +27,15 @@ function get(req, res) {
  * @returns {User}
  */
 function create(req, res, next) {
-  const user = new User({
-    username: req.body.username,
-    mobileNumber: req.body.mobileNumber
-  });
+	const user = new User({
+		username: req.body.username,
+		mobileNumber: req.body.mobileNumber
+	});
 
-  user.save()
-    .then(savedUser => res.json(savedUser))
-    .catch(e => next(e));
+	user
+		.save()
+		.then((savedUser) => res.json(savedUser))
+		.catch((e) => next(e));
 }
 
 /**
@@ -44,13 +45,14 @@ function create(req, res, next) {
  * @returns {User}
  */
 function update(req, res, next) {
-  const user = req.user;
-  user.username = req.body.username;
-  user.mobileNumber = req.body.mobileNumber;
+	const user = req.user;
+	user.username = req.body.username;
+	user.mobileNumber = req.body.mobileNumber;
 
-  user.save()
-    .then(savedUser => res.json(savedUser))
-    .catch(e => next(e));
+	user
+		.save()
+		.then((savedUser) => res.json(savedUser))
+		.catch((e) => next(e));
 }
 
 /**
@@ -60,10 +62,10 @@ function update(req, res, next) {
  * @returns {User[]}
  */
 function list(req, res, next) {
-  const { limit = 50, skip = 0 } = req.query;
-  User.list({ limit, skip })
-    .then(users => res.json(users))
-    .catch(e => next(e));
+	const { limit = 50, skip = 0 } = req.query;
+	User.list({ limit, skip })
+		.then((users) => res.json(users))
+		.catch((e) => next(e));
 }
 
 /**
@@ -71,10 +73,11 @@ function list(req, res, next) {
  * @returns {User}
  */
 function remove(req, res, next) {
-  const user = req.user;
-  user.remove()
-    .then(deletedUser => res.json(deletedUser))
-    .catch(e => next(e));
+	const user = req.user;
+	user
+		.remove()
+		.then((deletedUser) => res.json(deletedUser))
+		.catch((e) => next(e));
 }
 
 module.exports = { load, get, create, update, list, remove };
